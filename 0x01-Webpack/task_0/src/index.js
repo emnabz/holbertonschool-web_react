a20e5d61b689ca1dc85e429ca3c243fdb7c25e52
emnabz <1947@holbertonschool.com>
2022-02-11 13:23:55 +0100

    jquery

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -1,5 +1,6 @@
-import $ from "jquery";
 
-$("<p>Holberton Dashboard</p>").appendTo($('body'));
-$("<p>Dashboard data for the students</p>").appendTo($('body'));
-$("<p>Copyright - Holberton School</p>").appendTo($('body'));
+const $ = require('jquery');
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
