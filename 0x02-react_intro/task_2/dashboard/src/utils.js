bfc67726ebfcc7942fa5a76e42e5cb9ba3c8e0d8
emnabz <1947@holbertonschool.com>
2022-02-18 00:17:40 +0100

    email validation

---
+++ 0x02-react_intro/task_2/dashboard/src/utils.js
@@ -0,0 +1,6 @@
+export const getFullYear = () => {
+    return new Date().getFullYear();
+}
+export const getFooterCopy = (isIndex) => {
+    return isIndex ? "Holberton School" : "Holberton School main dashboard";
+}
